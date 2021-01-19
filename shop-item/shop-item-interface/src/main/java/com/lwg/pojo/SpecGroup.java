590b4428deebcd85d3f5a0088d43a93f11449aa2
lwg729 <1098372951@qq.com>
2021-01-20 00:52:05 +0800

    商品规格参数实体类

---
+++ shop-item/shop-item-interface/src/main/java/com/lwg/pojo/SpecGroup.java
@@ -0,0 +1,27 @@
+package com.lwg.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "tb_spec_group")
+public class SpecGroup {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long cid;
+
+    private String name;
+
+    @Transient
+    private List<SpecParam> params;
+
+}
---
+++ shop-item/shop-item-interface/src/main/java/com/lwg/pojo/SpecParam.java
@@ -0,0 +1,27 @@
+package com.lwg.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "tb_spec_param")
+public class SpecParam {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long cid;
+    private Long groupId;
+    private String name;
+    @Column(name = "`numeric`")
+    private Boolean numeric;
+    private String unit;
+    private Boolean generic;
+    private Boolean searching;
+    private String segments;
+    
+}
