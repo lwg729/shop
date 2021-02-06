b8144c78c7431c72e856adad966a6756c6b4c0db
lwg729 <1098372951@qq.com>
2021-02-06 17:37:24 +0800

    搭建elasticsearch

---
+++ shop-search/src/main/java/com/lwg/search/ShopSearchApplication.java
@@ -0,0 +1,22 @@
+package com.lwg.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * 功能描述：
+ *
+ * @Author: lwg
+ * @Date: 2021/2/6 17:12
+ */
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class ShopSearchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShopSearchApplication.class,args);
+    }
+}
