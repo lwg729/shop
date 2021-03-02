04a1357fa3a67bfb2fdbb638727a16be9b1df144
lwg729 <1098372951@qq.com>
2021-03-03 00:13:17 +0800

    构建购物车微服务

---
+++ shop-cart/src/main/java/com/lwg/ShopCartApplication.java
@@ -0,0 +1,22 @@
+package com.lwg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * 功能描述：
+ *
+ * @Author: lwg
+ * @Date: 2021/3/3 0:08
+ */
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
+public class ShopCartApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShopCartApplication.class,args);
+    }
+}
