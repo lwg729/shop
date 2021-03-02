47da1605010d086bb4bfe1ae3d6227d0e7487dec
lwg729 <1098372951@qq.com>
2021-03-02 23:30:23 +0800

    用户刷新页面时更新cookie

--- shop-auth/shop-auth-service/src/main/java/com/lwg/controller/AuthController.java
+++ shop-auth/shop-auth-service/src/main/java/com/lwg/controller/AuthController.java
@@ -2,16 +2,15 @@ package com.lwg.controller;
 
 import com.lwg.common.utils.CookieUtils;
 import com.lwg.config.JwtProperties;
-import com.lwg.service.AuthService;
+import com.lwg.entity.UserInfo;
 import com.lwg.service.impl.AuthServiceImpl;
+import com.lwg.utils.JwtUtils;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -51,13 +50,39 @@ public class AuthController {
         //登录校验
         String token = authService.accredit(username, password);
 
-        if (StringUtils.isBlank(token)){
+        if (StringUtils.isBlank(token)) {
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
 
         //将token写入cookie 并指定httpOnly为true,防止通过JS获取和修改
-        CookieUtils.setCookie(request,response,jwtProperties.getCookieName(),token,jwtProperties.getExpire()*60);
+        CookieUtils.setCookie(request, response, jwtProperties.getCookieName(), token, jwtProperties.getExpire() * 60);
 
         return ResponseEntity.ok(null);
+    }
+
+    /**
+     * 校验用户信息
+     *
+     * @param token
+     * @return
+     */
+    @GetMapping("verify")
+    public ResponseEntity<UserInfo> verifyUser(@CookieValue("LW_TOKEN") String token,HttpServletRequest request,
+                                               HttpServletResponse response) {
+        try {
+            //从token中解析token信息
+            UserInfo userInfo = JwtUtils.getInfoFromToken(token, jwtProperties.getPublicKey());
+            //解析成功要重新刷新token
+            JwtUtils.generateToken(userInfo, jwtProperties.getPrivateKey(), jwtProperties.getExpire());
+
+            //更新cookie中的token
+            CookieUtils.setCookie(request,response,jwtProperties.getCookieName(),token,jwtProperties.getExpire()*60);
+            //解析成功返回用户信息
+            return ResponseEntity.ok(userInfo);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // 出现异常则，响应500
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
     }
 }
